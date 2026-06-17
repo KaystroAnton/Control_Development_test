b76e29c60ca3dcc016d236c2c2677f10ce0e5bd2
KaystroAnton <anton.kaystro@mail.ru>
2026-06-17 11:07:22 +0300

    lifespqn added

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,27 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from api import router as api_router
+from core.models import db_helper
 from core.config import settings
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    yield
+    #shutdown
+    print("dispose_engine")
+    await db_helper.dispose()
 
-app = FastAPI()
-app.include_router(api_router,
+main_app = FastAPI(
+    lifespan = lifespan,
+)
+main_app.include_router(api_router,
                    prefix= settings.api.prefix,
 )
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", 
+    uvicorn.run("main:main_app", 
                 reload=settings.run.reload, 
                 host = settings.run.host, 
                 port = settings.run.port,
