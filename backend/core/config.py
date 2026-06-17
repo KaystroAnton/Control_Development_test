3fcdc93f1bec75e80fdd68ccace9059e0a5a83d2
KaystroAnton <anton.kaystro@mail.ru>
2026-06-17 14:40:50 +0300

    fix config db url+ added naming convantion db config for alembic

--- backend/core/config.py
+++ backend/core/config.py
@@ -11,15 +11,23 @@ class Api_Prefix(BaseModel):
     prefix: str = "/api"
 
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn 
+    url: str 
     echo: bool = False
     echo_pool: bool = False
     max_overflow: int = 50
     pool_size: int = 50
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 class Settings(BaseSettings):
     model_config: SettingsConfigDict = SettingsConfigDict(
-        env_file=("backend/.env.template","backend/.env"),
+        env_file=(".env.template",".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
