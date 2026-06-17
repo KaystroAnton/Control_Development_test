f69742a5d891f02c863d65303d4ff00e0fa5e2af
KaystroAnton <anton.kaystro@mail.ru>
2026-06-17 11:06:09 +0300

    added config for db

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RunCongig(BaseModel):
     host: str = "0.0.0.0"
@@ -11,16 +11,22 @@ class Api_Prefix(BaseModel):
     prefix: str = "/api"
 
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn
+    url: PostgresDsn 
     echo: bool = False
     echo_pool: bool = False
     max_overflow: int = 50
     pool_size: int = 50
 
 class Settings(BaseSettings):
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file=("backend/.env.template","backend/.env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunCongig = RunCongig()
     api: Api_Prefix = Api_Prefix()
-    db: DatabaseConfig
+    db: DatabaseConfig 
 
 # инициализация
 settings = Settings()
